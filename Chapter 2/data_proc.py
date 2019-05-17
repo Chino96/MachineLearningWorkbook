8eedbc05124cf88de2fec6c44b64857939cfaa5c
chino <chino.s.ugwumadu@gmail.com>
2019-05-17 11:34:02 -0400

    chapter 2 updates

--- Chapter 2/data_proc.py
+++ Chapter 2/data_proc.py
@@ -1,6 +1,8 @@
 import os
 import tarfile
 import pandas as pd
+import numpy as np
+from zlib import crc32
 from six.moves import urllib
 
 DOWNLOAD_ROOT = 'https://raw.githubusercontent.com/ageron/handson-ml/master/'
@@ -17,6 +19,18 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 	housing_tgz.extractall(path=housing_path)
 	housing_tgz.close()
 
+
 def load_housing_data(housing_path=HOUSING_PATH):
 	csv_path = os.path.join(housing_path, 'housing.csv')
 	return pd.read_csv(csv_path)
+
+
+def test_set_check(identifier, test_ratio):
+	return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+
+def split_train_test(data, test_ratio, id_column):
+	ids = data[id_column]
+	in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+	return data.loc[~in_test_set], data.loc[in_test_set]
+
--- Chapter 2/runner.py
+++ Chapter 2/runner.py
@@ -1,6 +1,9 @@
-from data_fetcher import load_housing_data
+from data_proc import load_housing_data, split_train_test
 import matplotlib.pyplot as plt
 
 housing = load_housing_data()
-housing.hist(bins=50, figsize=(20,15))
-plt.show()
+housing_with_id = housing.reset_index()
+train, test = split_train_test(housing_with_id, .20, 'index')
+print(len(test))
+#housing.hist(bins=50, figsize=(20,15))
+#plt.show()
