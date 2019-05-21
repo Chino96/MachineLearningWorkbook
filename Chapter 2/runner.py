aeafb4751f3306beb6577f1098261ed79651c362
chino <chino.s.ugwumadu@gmail.com>
2019-05-21 09:47:43 -0400

    chapter 2 updates

--- Chapter 2/runner.py
+++ Chapter 2/runner.py
@@ -1,9 +1,21 @@
 from data_proc import load_housing_data, split_train_test
+from sklearn.model_selection import StratifiedShuffleSplit
+import numpy as np
 import matplotlib.pyplot as plt
 
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 housing = load_housing_data()
 housing_with_id = housing.reset_index()
+
+housing['income_cat'] = np.ceil(housing['median_income']/1.5)
+housing['income_cat'].where(housing['income_cat'] < 5, 5.0, inplace=True)
+
+for train_index, test_index in split.split(housing, housing['income_cat']):
+	strat_train_set = housing.loc[train_index]
+	strat_test_set = housing.loc[test_index]
+
+print(strat_test_set['income_cat'].value_counts() / len(strat_test_set))
+
 train, test = split_train_test(housing_with_id, .20, 'index')
-print(len(test))
 #housing.hist(bins=50, figsize=(20,15))
 #plt.show()
